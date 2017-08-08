a44eac34ba68f54cd7663bc60b3639809ed7e807
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-08 14:09:57 -0400

    Added a GET for ids and habitats

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 const express = require('express')
-const bp = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const myDinos = [
   {
-    id: '1',
+    id: 1,
     name: 'Aardonyx',
     weight: '1,000 lbs',
     habitats: 'Woodlands'
   },
   {
-    id: '2',
+    id: 2,
     name: 'Diceratops',
     weight: '2-3 tons',
     habitats: 'Woodlands'
   },
   {
-    id: '3',
+    id: 3,
     name: 'Fabrosaurus',
     weight: '10-20 lbs',
     habitats: 'Woodlands'
   },
   {
-    id: '4',
+    id: 4,
     name: 'Hexinlusaurus',
     weight: '25 lbs',
     habitats: 'Woodlands'
   },
   {
-    id: '5',
+    id: 5,
     name: 'Kulindadromeus',
     weight: '20-30 lbs',
     habitats: 'Plains'
@@ -42,6 +44,11 @@ app.get('/api/dinos', function(req, res) {
 app.get('/api/dinos/:id', (req, res) => {
   const oneDino = myDinos.find(dino => dino.id === parseInt(req.params.id))
   res.json(oneDino)
+})
+
+app.get('/api/dinos/:id/habitats', (req, res) => {
+  const oneDino = myDinos.find(dino => dino.id === parseInt(req.params.id))
+  res.json(oneDino.habitats)
 })
 
 app.listen(3000, function() {
